67ac2dfcac7a4c05a19754ae2070b1fa2053ad77
hoanngkhaidev <hoangkhaidev@gmail.com>
2022-09-29 12:07:13 +0700

    fix-bug-comma

--- src/views/TransactionDetails/TransactionDetailsTable.js
+++ src/views/TransactionDetails/TransactionDetailsTable.js
@@ -11,16 +11,14 @@ import NoPermissionPage from "views/NoPermissionPage/NoPermissionPage";
 import Loading from "views/Loading/Loading";
 import TableComponent from "views/TableComponent/TableComponent";
 import RoundDetail from "./RoundDetail";
+import { formatNumberWithComma } from "utils/function";
 
 const TransactionDetailsTable = ({roundId}) => {
   const [pagination, setPagination] = useState({
     page: 1,
     page_size: 30
   });
-  const formatNumber = (num) => {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-  }
-
+ 
   const { dataResponse, isLoading, isHasPermission } = useFetchData(
     `/api/transaction/round/${roundId}/details`,
     null
@@ -64,28 +62,19 @@ const TransactionDetailsTable = ({roundId}) => {
       data_field: "debit",
       column_name: "Debit",
       align: "right",
-      formatter: (cell, row) => {
-        let cellFormat = formatNumber(cell);
-        return cellFormat;
-      }
+      formatter: cell => formatNumberWithComma(cell)
     },
     {
       data_field: "credit",
       column_name: "Credit",
       align: "right",
-      formatter: (cell, row) => {
-        let cellFormat = formatNumber(cell);
-        return cellFormat;
-      }
+      formatter: cell => formatNumberWithComma(cell)
     },
     {
       data_field: "balance_after",
       column_name: "Balance",
       align: "right",
-      formatter: (cell, row) => {
-        let cellFormat = formatNumber(cell);
-        return cellFormat;
-      }
+      formatter: cell => formatNumberWithComma(cell)
     }
   ];
 
